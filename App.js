a5bf4fb651d965cce7cc50fd991ccabec28883ec
Matt Brown <matthewbrown0798@gmail.com>
2021-10-06 00:01:34 -0400

    themeing added

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Platform, StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components/native";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { theme } from "./src/infrastructure/theme";
 
 const isAndroid = Platform.OS === "android";
 
 export default function App() {
   return (
     <>
-      <RestaurantsScreen />
-      <ExpoStatusBar style="auto" />
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+        <ExpoStatusBar style="auto" />
+      </ThemeProvider>
     </>
   );
 }
--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -25,15 +25,15 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
 
 // STYLES
 const RestaurantCard = styled(Card)`
-  background-color: white;
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 const RestaurantCardCover = styled(Card.Cover)`
-  padding: 20px;
-  color: red;
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 const Title = styled(Text)`
-  padding: 16px;
-  color: red;
+  padding: ${(props) => props.theme.space[3]};
+  color: ${(props) => props.theme.colors.ui.primary};
 `;
